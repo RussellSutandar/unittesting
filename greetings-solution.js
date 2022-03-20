9cecb59cf6add4bbbba85ce3ba6b57c09a504cbe
RussellSutandar <russellsutandar@gmail.com>
2022-03-20 13:35:58 +0700

    update

--- greetings-solution.js
+++ greetings-solution.js
@@ -1,2 +1,27 @@
 // This is an example of the greet() function covering requirements layed out in the Unit Testing Practice activity
+function Greet(name) {
+    if (name == null) {
+        return "Hello there!"
+    }
+    else if(typeof name === typeof []) 
+    {if (name.length > 1) {
+        let names = ""
+        name.forEach((element) => {
+            names = names + ", " + element
+        })
+    
+        return "Hello" + names
+    }}
+    else if (name == name.toUpperCase()) {
+        return "HELLO " + name + "!"
+    }
 
+    else {
+        return "Hello, " + name
+    }
+}
+
+console.log(Greet("Russell"))
+console.log(Greet())
+console.log(Greet("RUSSELL"))
+console.log(Greet(["a", "b"]))
--- greetings.spec.js
+++ greetings.spec.js
@@ -3,4 +3,22 @@
 // run the command: npm install to setup the testing framework
 // Thn run the command: npm run test
 // The tests should run successfully
+import Greet from `./greetings-solution`
 
+describe('test greet()', function () {
+    it('should greet a name', function () {
+        expect(Greet('Elizabeth')).toEqual('Hello, Elizabeth');
+    })
+
+    it('should greet a name', function () {
+        expect(Greet('ELIZABETH')).toEqual('HELLO ELIZABETH!');
+    })
+
+    it('should greet a name', function () {
+        expect(Greet()).toEqual('Hello there!');
+    })
+
+    it('should greet a name', function () {
+        expect(Greet(['a', 'b'])).toEqual('Hello, a, b');
+    })
+})
