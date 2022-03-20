bda80b90ba9a5708deed652d1561780deed23805
tavleenduggal <60790884+tavleenduggal@users.noreply.github.com>
2022-03-20 10:37:13 +0530

    Add files via upload

---
+++ greetings-solution.js
@@ -0,0 +1,2 @@
+// This is an example of the greet() function covering requirements layed out in the Unit Testing Practice activity
+
---
+++ greetings.spec.js
@@ -0,0 +1,6 @@
+// Unit tests to cover the greet() function
+// To run these tests, make sure you're in the folder containing this file
+// run the command: npm install to setup the testing framework
+// Thn run the command: npm run test
+// The tests should run successfully
+
